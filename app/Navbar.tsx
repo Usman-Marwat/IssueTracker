c287e6d366312c55fde734ed445c4408c35241a4
Usman-Marwat <usk895907a2@gmail.com>
2023-12-23 20:44:56 +0500

    Style the active link in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
+import classnames from 'classnames';
 
 export default () => {
+	const currentPath = usePathname();
+
 	const links = [
 		{ label: 'Dashboard', href: '/' },
 		{ label: 'Issues', href: '/issues' },
@@ -17,7 +23,11 @@ export default () => {
 				{links.map((link) => (
 					<Link
 						key={link.href}
-						className="text-zinc-500 hover:text-zinc-800 transition-colors"
+						className={classnames({
+							'text-zinc-900': link.href === currentPath,
+							'text-zinc-500': link.href !== currentPath,
+							'hover:text-zinc-800 transition-colors': true,
+						})}
 						href={link.href}
 					>
 						{link.label}
@@ -27,3 +37,8 @@ export default () => {
 		</nav>
 	);
 };
+
+/*
+usePathname() is dependent on browser apis, we have to convert this component to a client 
+component because we can only access browser apis in client component
+*/
