be48992bbd0a15e3d59acde2b81ad25f73408834
Usman-Marwat <usk895907a2@gmail.com>
2023-12-25 15:25:22 +0500

    Extract the error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -13,6 +13,7 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/api/validationSchemas';
 import axios from 'axios';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -49,11 +50,7 @@ export default () => {
 				<TextField.Root>
 					<TextField.Input placeholder="Title" {...register('title')} />
 				</TextField.Root>
-				{errors.title && (
-					<Text color="red" as="p">
-						{errors.title.message}
-					</Text>
-				)}
+				<ErrorMessage>{errors.title?.message}</ErrorMessage>
 				<Controller
 					name="description"
 					control={control}
@@ -61,11 +58,9 @@ export default () => {
 						<SimpleMDE placeholder="Description" {...field} />
 					)}
 				/>
-				{errors.description && (
-					<Text color="red" as="p">
-						{errors.description.message}
-					</Text>
-				)}
+
+				<ErrorMessage>{errors.description?.message}</ErrorMessage>
+
 				<Button>Submit New Issue</Button>
 			</form>
 		</div>
