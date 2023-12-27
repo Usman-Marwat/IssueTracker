1e0b10f576c5140c0d5e70cb6bf2137d0d778265
Usman-Marwat <usk895907a2@gmail.com>
2023-12-27 13:15:34 +0500

    Improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -8,21 +8,17 @@ import { Issue } from '@prisma/client';
 import { Button, Callout, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
-import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
 import { z } from 'zod';
-
-//Dynamically loading component or lazy loading
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-	ssr: false,
-});
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
 export default ({ issue }: { issue?: Issue }) => {
 	const router = useRouter();
+	const [error, setError] = useState('');
 	const {
 		register,
 		control,
@@ -31,7 +27,6 @@ export default ({ issue }: { issue?: Issue }) => {
 	} = useForm<IssueFormData>({
 		resolver: zodResolver(issueSchema),
 	});
-	const [error, setError] = useState('');
 
 	const onSubmit = handleSubmit(async (data) => {
 		try {
