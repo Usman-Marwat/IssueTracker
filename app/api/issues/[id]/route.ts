3e1d333993e630e3fac6ab99a1b00c322f837cc2
Usman-Marwat <usk895907a2@gmail.com>
2023-12-27 17:02:35 +0500

    Secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,12 +1,17 @@
-import { issueSchema } from '@/app/validationSchemas';
-import prisma from '@/prisma/client';
 import { NextRequest as req, NextResponse as res } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { issueSchema } from '@/app/validationSchemas';
+import authOptions from '@/app/auth/AuthOptions';
+import prisma from '@/prisma/client';
 
 interface Params {
 	params: { id: string };
 }
 
 export async function PATCH(request: req, { params }: Params) {
+	const session = await getServerSession(authOptions);
+	if (!session) return res.json({}, { status: 401 });
+
 	const body = await request.json();
 	const v = issueSchema.safeParse(body);
 	if (!v.success) return res.json(v.error.format(), { status: 400 });
@@ -23,6 +28,9 @@ export async function PATCH(request: req, { params }: Params) {
 }
 
 export async function DELETE(request: req, { params }: Params) {
+	const session = await getServerSession(authOptions);
+	if (!session) return res.json({}, { status: 401 });
+
 	const issue = await prisma.issue.findUnique({ where: { id: +params.id } });
 	if (!issue) return res.json({ error: 'Invalid issue' }, { status: 404 });
 
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,16 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
-import prisma from '@/prisma/client';
+import { NextRequest as req, NextResponse as res } from 'next/server';
+import { getServerSession } from 'next-auth';
 import { issueSchema } from '../../validationSchemas';
+import authOptions from '@/app/auth/AuthOptions';
+import prisma from '@/prisma/client';
 
-export async function POST(request: NextRequest) {
+export async function POST(request: req) {
+	const session = await getServerSession(authOptions);
+	if (!session) return res.json({}, { status: 401 });
+
 	const body = await request.json();
-	const validation = issueSchema.safeParse(body);
-	if (!validation.success)
-		return NextResponse.json(validation.error.format(), { status: 400 });
+	const v = issueSchema.safeParse(body);
+	if (!v.success) return res.json(v.error.format(), { status: 400 });
 
 	const newIssue = await prisma.issue.create({
 		data: { title: body.title, description: body.description },
 	});
 
-	return NextResponse.json(newIssue, { status: 201 });
+	return res.json(newIssue, { status: 201 });
 }
