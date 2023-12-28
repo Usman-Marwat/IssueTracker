0e01b1881d61789ef79644972cef285d029f0aa2
Usman-Marwat <usk895907a2@gmail.com>
2023-12-28 11:17:06 +0500

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -29,13 +29,16 @@ interface Props {
 }
 
 export default async ({ searchParams }: Props) => {
-	const statuses = Object.values(Status);
-	const status = statuses.includes(searchParams.status)
+	const status = Object.values(Status).includes(searchParams.status)
 		? searchParams.status
+		: undefined;
+	const orderBy = columns.map((col) => col.value).includes(searchParams.orderBy)
+		? { [searchParams.orderBy]: 'asc' }
 		: undefined;
 
 	const issues = await prisma.issue.findMany({
 		where: { status },
+		orderBy,
 	});
 
 	return (
