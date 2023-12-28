73fa01c4f249fe0ffb1cca46a0d6a1c16a9c5664
Usman-Marwat <usk895907a2@gmail.com>
2023-12-28 10:21:57 +0500

    Filter Issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,10 +1,22 @@
 import { Table } from '@radix-ui/themes';
+import { Link, IssueStatusBadge } from '../../components';
+import { Status } from '@prisma/client';
 import prisma from '@/prisma/client';
 import IssueActions from './IssueActions';
-import { Link, IssueStatusBadge } from '../../components';
 
-export default async () => {
-	const issues = await prisma.issue.findMany();
+interface Props {
+	searchParams: { status: Status };
+}
+
+export default async ({ searchParams }: Props) => {
+	const statuses = Object.values(Status);
+	const status = statuses.includes(searchParams.status)
+		? searchParams.status
+		: undefined;
+
+	const issues = await prisma.issue.findMany({
+		where: { status },
+	});
 
 	return (
 		<div>
